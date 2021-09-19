369777e6b4eb0ad2651dafee628f7afde30d1a74
Santiago Diaz <santi.diazl@icloud.com>
2021-09-19 01:44:41 -0500

    only accept http protocols

--- controller.js
+++ controller.js
@@ -4,64 +4,62 @@ const Counter = require('./models/counter');
 
 const dns = require('dns');
 const dnsPromises = dns.promises;
-
-// const verifyWWW = (req, res, next) => { // check that URL provided contains www
-//   console.log(`URL entered: ${req.body.url}`);
-//   // fcc requirement
-//   const url = req.body.url;
-//   if (!url.includes('www')) {
-//     console.error('www not in URL provided');
-//     return res.json({error: 'invalid url'});
-//   }
-//   next();
-// };
-
-const parseURL = (req, res, next) => { // checks that URL is valid
-  console.log(`URL entered: ${req.body.url}`);
-  try { // if  URL parsing fails, will be caught by catch
+// checks that input URL is valid
+const parseURL = (req, res, next) => {
+  try {
+    // if  URL parsing fails, will be caught by catch
     const url = new URL(req.body.url);
-    // remove www for dns.lookup() in next middleware function
-    req.body.hostName = url.hostname.replace('www.', '');
+    if (!url.protocol.startsWith('http')) { // only accept http protocols
+      throw new Error('invalid protocol, must be http[s]');
+    }
+    // if www in host name remove for dns.lookup() in next middleware function
+    let hostName = url.hostname;
+    if (hostName.startsWith('www')) {
+      hostName = hostName.replace('www.', '');
+    }
+    req.body.hostName = hostName;
     next();
   } catch (err) {
     console.error(err);
     return res.json({error: 'invalid url'});
   }
 };
-
-const resolveHost = (req, res, next) => { // checks that URL points to real host
-  dnsPromises.lookup(req.body.hostName).catch((err) => {
+// checks that input URL resolves
+const resolveHost = (req, res, next) => {
+  dnsPromises.lookup(req.body.hostName).then((resp) => {
+    console.log(resp);
+    next();
+  }).catch((err) => { // error means not found
     console.error(err);
-    return res.json({error: 'invalid url'});
+    res.json({error: 'invalid url'});
   });
-  next();
 };
-
-const processRequest = async (req, res, next) => {
-  count = await Counter.findOneAndUpdate( // gets current count for short URL
+// generates a short URL: http://example.com -> api/shorturl/13
+const processReq = async (req, res, next) => {
+  // get currentCount for short url (i.e /api/shorturl/currentCount)
+  currentCount = await Counter.findOneAndUpdate(
       {_id: 'URL Counter'},
       {$inc: {seq_value: 1}},
       {returnNewDocument: true,
         upsert: true},
   ).exec();
-
-  const url = new URLS({
+  // save original and short
+  const newURL = await new URLS({
     original_url: req.body.url,
     short_url: count.seq_value,
-  });
-
-  const newURL = await url.save();
-
+  }).save();
+  // respond
   res.json({
     original_url: newURL.original_url,
     short_url: newURL.short_url,
   });
 };
 
-const postURL = [/*verifyWWW,*/ parseURL, resolveHost, processRequest];
-
-const getURL = async (req, res, next) => { // redirects to original URL
-  const url = await URLS.findOne({short_url: req.params.short_url},
+const postURL = [parseURL, resolveHost, processReq];
+// redirects to original URL: api/shorturl/13 -> http://example.com
+const getURL = async (req, res, next) => {
+  // finds corresponding URL
+  const url = await URLS.findOne({short_url: req.params.short_url}, // find URL
       'original_url').exec();
   res.redirect(url.original_url);
 };
--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // Composed function to wrap around async routes, if there is an error with
 // async routes, it well be caught by Promise.catch(), with next function
-// Being passed to it
+// Passed to it
 // Shutout to David Sag of ITNEXT for elegant solution:
 // (https://itnext.io/using-async-routes-with-express-bcde8ead1de8)
 const asyncRoute = (route) => (req, res, next) =>
@@ -12,11 +12,11 @@ const asyncRoute = (route) => (req, res, next) =>
 // Controller
 const controller = require('./controller');
 const postFunctions = controller.postURL;
-[/*verifyWWW,*/parseURL, resolveHost, processRequest] =
-  [postFunctions[0], postFunctions[1], postFunctions[2]/*, postFunctions[3]*/];
+[parseURL, resolveHost, processRequest] =
+[postFunctions[0], postFunctions[1], postFunctions[2]];
 
 // POST - new URL
-router.post('/', /*verifyWWW,*/ parseURL, resolveHost, asyncRoute(processRequest));
+router.post('/', parseURL, resolveHost, asyncRoute(processRequest));
 
 // GET -  short URL and redirect
 router.get('/:short_url', controller.getURL);
